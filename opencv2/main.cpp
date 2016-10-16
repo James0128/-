c8bff53023f2b6425d744a656d9edb27640f24c8
James0128 <782234346@qq.com>
2016-10-16 16:56:49 +0800

    Initial Commit

---
+++ opencv2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv2
+//
+//  Created by 逸帆 杨 on 16/10/16.
+//  Copyright © 2016年 逸帆 杨. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
